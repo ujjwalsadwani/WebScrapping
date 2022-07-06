9c25c224db4b47ed2ea97735d51d084227bee0d5
ujjwalsadwani <69423561+ujjwalsadwani@users.noreply.github.com>
2022-07-06 13:26:19 +0530

    Add files via upload
    Stock Market data extraction

---
+++ market data.py
@@ -0,0 +1,22 @@
+import string
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+import sys
+
+api_key = "6TB81TGNIUQ7RWG9"
+
+series = TimeSeries(key=api_key, output_format="pandas")
+sym = input("Enter the 'SYMBOL' of the Stock:\n")
+try:
+    data, meta_data = series.get_intraday(symbol=sym, interval="1min", outputsize="full")
+    print(data)
+except Exception as e:
+    print(f"Oops! {e.__class__} occurred.")
+
+i=1
+while i==1:
+    data, meta_data = series.get_intraday(symbol=sym, interval="1min", outputsize="full")
+    print(data)
+    data.to_csv("output.csv")
+    time.sleep(30)
